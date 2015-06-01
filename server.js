b3dfad51fae122bc00f5e36600d84bedc166fc3e
Justin Hill <justinmhill@gmail.com>
2015-06-01 17:25:22 -0400

    Add ability to query spotify api

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var unirest = require('unirest');
+var events = require('events');
+
+var app = express()
+app.use(express.static('public'));
+
+var getFromApi = function (endpoint, args) {
+	var emitter = new events.EventEmitter();
+	unirest.get('https://api.spotify.com/v1/' + endpoint)
+		.qs(args)
+		.end(function(response){
+			emitter.emit('end', response.body);
+		});
+	return emitter;
+};
+
+app.get('/search/:name', function(req, res) {
+	var searchReq = getFromApi('search', {
+		q: req.params.name,
+		limit: 1,
+		type: 'artist'
+	});
+
+	searchReq.on('end', function(item) {
+		var artist = item.artists.items[0];
+		res.json(artist);
+	});
+
+	searchReq.on('error', function() {
+		res.sendStatus(404);
+	});
+});
+
+app.listen(8080);

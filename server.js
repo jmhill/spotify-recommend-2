9031d2372cd8bc235d2a3ec76e3734deb1cbe2bf
Justin M. Hill <justinmhill@gmail.com>
2016-05-14 20:34:10 -0400

    Add console log and export of app for testing

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ var port = process.env.PORT || 8080;
 var express = require('./config/express');
 
 var app = express();
+
 app.listen(port);
+console.log('app running on port: ' + port);
+
+exports.app = app;
